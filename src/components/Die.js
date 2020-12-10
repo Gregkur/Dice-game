16db4ee93093e3bfbedee80cec6247bb1d3c65c3
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-12-10 19:27:09 +0100

    added a dice component

---
+++ src/components/Die.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import '../style/Die.css'
+
+export default class Die extends Component {
+  render() {
+    return (
+      <div>
+        <i class="fas fa-dice-one"></i>
+        <i class="fas fa-dice-two"></i>
+      </div>
+    )
+  }
+}

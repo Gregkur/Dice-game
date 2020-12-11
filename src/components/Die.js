8d05b19444562470df0d9a17da79104a033bed87
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-12-11 19:06:38 +0100

    added simple animations for the dice

--- src/components/Die.js
+++ src/components/Die.js
@@ -5,7 +5,11 @@ export default class Die extends Component {
   render() {
     return (
       <div>
-        <i className={`fas fa-dice-${this.props.face}`}></i>
+        <i
+          className={`fas fa-dice-${this.props.face} ${
+            this.props.rolling ? "wobble-hor-bottom" : ""
+          }`}
+        ></i>
       </div>
     );
   }
--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -14,7 +14,7 @@ export default class RollDice extends Component {
     let die2 = this.props.sides[
       Math.floor(Math.random() * this.props.sides.length)
     ];
-    this.state = { die1: die1, die2: die2 };
+    this.state = { die1: die1, die2: die2, rolling: false };
     //bindging the function to an instance
     this.roll = this.roll.bind(this);
   }
@@ -27,15 +27,21 @@ export default class RollDice extends Component {
       Math.floor(Math.random() * this.props.sides.length)
     ];
     //set state
-    this.setState({ die1: newDie1, die2: newDie2 });
+    this.setState({ die1: newDie1, die2: newDie2, rolling: true });
+    // wait a secong then set rolling to false
+    setTimeout(() => {
+      this.setState({ rolling: false });
+    }, 1000);
   };
   render() {
     return (
       <div className="RollDice-container">
-        <Die face={this.state.die1} />
-        <Die face={this.state.die2} />
+        <Die face={this.state.die1} rolling={this.state.rolling} />
+        <Die face={this.state.die2} rolling={this.state.rolling} />
         <div className="RollDice-button">
-          <button onClick={this.roll}>Roll the dice!</button>
+          <button onClick={this.roll} disabled={this.state.rolling}>
+            {this.state.rolling ? "Rollling.." : "Roll Dice!"}
+          </button>
         </div>
       </div>
     );
